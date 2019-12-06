52480781177e1ca8d2e891e90c101f245333e304
Maido Käära <m@maido.io>
2019-12-06 14:02:06 +0200

    Use tailrec

--- src/main/kotlin/advent/Advent6.kt
+++ src/main/kotlin/advent/Advent6.kt
@@ -2,23 +2,18 @@ package advent
 
 class Advent6(text: List<String> = readFile("/advent6")) : Advent {
 
-    private val orbits = text.map { val (a, b) = it.split(')'); b to a }.toMap()
+    private val orbits = text.map { it.split(')').let { (a, b) -> b to a } }.toMap()
 
-    override fun firstTask() = orbits.values.map { planets(it).count() }.sum()
+    override fun firstTask() = orbits.values.map { planets(it).count() + 1 }.sum()
 
-    override fun secondTask() = transfers()
-
-    private fun transfers(): Int {
-        val myPlanets = planets("YOU")
-        val sanPlanets = planets("SAN")
-        return myPlanets.intersect(sanPlanets).let {
-            myPlanets.count { m -> !it.contains(m) } + sanPlanets.count { s -> !it.contains(s) }
+    override fun secondTask() = (planets("YOU") to planets("SAN"))
+        .let { (you, san) ->
+            you.count() + san.count() - 2 * you.intersect(san).count()
         }
-    }
 
-    private fun planets(planet: String): List<String> {
-        if (planet == "COM") return listOf("COM")
+    private tailrec fun planets(planet: String, acc: List<String> = emptyList()): List<String> {
+        if (planet == "COM") return acc
         val orbitsPlanet = orbits[planet] ?: error("No planet")
-        return listOf(orbitsPlanet) + planets(orbitsPlanet)
+        return planets(orbitsPlanet, listOf(orbitsPlanet) + acc)
     }
 }
